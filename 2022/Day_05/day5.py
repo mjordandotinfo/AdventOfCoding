75f98f723305127b4abfc349dd30f05173370f38
mjordan <matt@mjordan.info>
2022-12-06 00:12:45 -0600

    Day 5

---
+++ 2022/Day_05/day5.py
@@ -0,0 +1,31 @@
+f = open('./2022/Day_05/input.txt', 'r')
+lines = [line.strip('\n') for line in f]
+f.close()
+
+def getStacks(lines):
+    n = int((len(lines[0]) + 1) / 4)
+    stacks = [[] for i in range(n)]
+    for line in lines:
+        if line:
+            for i in range(n):
+                char = line[i*4 + 2 - 1]
+                if char.isalpha():
+                    stacks[i].insert(0,char)
+        else:
+            break
+    
+    return stacks
+
+def getInstructions(lines, stacks):
+    for line in lines:
+        [amt, src, dst] = [int(x) for x in line.split() if x.isnumeric()]
+        for _ in range(amt):
+            stacks[dst-1].append(stacks[src-1].pop())
+
+    return ''.join([x[len(x)-1] for x in stacks])
+    
+stacks = getStacks(lines)
+maxStack = max([len(x) for x in stacks])
+code = getInstructions(lines[maxStack+2:], stacks)
+
+print(code)
